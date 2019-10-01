70d63f4a69321efe9e9762e35730266b0ff60fad
Serge Lapraye <slapraye@la-providence.net>
2019-10-01 15:07:36 +0200

    Commit 1

---
+++ appli.h
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------
+
+#ifndef appliH
+#define appliH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <vector>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// Composants gérés par l'EDI
+	TButton *ButtonSend;
+	TButton *ButtonReceive;
+	TMemo *Memo1;
+	TEdit *Edit1;
+	void __fastcall ButtonSendExit(TObject *Sender);
+	void __fastcall ButtonSendClick(TObject *Sender);
+	void __fastcall ButtonReceiveClick(TObject *Sender);
+private:	// Déclarations utilisateur
+	HANDLE com;
+	//OVERLAPPED over;
+	//langlace ss
+	std::vector<char> received;
+public:		// Déclarations utilisateur
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
